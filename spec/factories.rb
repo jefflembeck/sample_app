883802d05dc06f2f95b8dea31a1e84b89d6e52cd
Jeff Lembeck <jlembeck@gmail.com>
2012-05-23 12:41:25 -0700

    Changing password to match the minimum 6 characters.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 	factory :user do
 		name		 "Andy Matthews"
 		email		"andy@goba.mobi"
-		password "goba"
-		password_confirmation "goba"
+		password "gobaaa"
+		password_confirmation "gobaaa"
 	end
 end
